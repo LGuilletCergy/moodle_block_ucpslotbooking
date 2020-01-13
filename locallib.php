57c6c20ade993ef71fefe72a741e8852e73ddd24
LGuillet <laurent.guillet@u-cergy.fr>
2020-01-13 09:22:46 +0100

    Bug correction

--- locallib.php
+++ locallib.php
@@ -41,25 +41,25 @@ function displaydate($datetext) {
 
         $day = "";
 
-        if (date("l", mktime(0,0,0, $dateparts[2], $dateparts[1], $dateparts[0])) == "Monday") {
+        if (date("l", mktime(0,0,0, $dateparts[1], $dateparts[2], $dateparts[0])) == "Monday") {
 
             $day = "Lundi";
-        } else if (date("l", mktime(0,0,0, $dateparts[2], $dateparts[1], $dateparts[0])) == "Tuesday") {
+        } else if (date("l", mktime(0,0,0, $dateparts[1], $dateparts[2], $dateparts[0])) == "Tuesday") {
 
             $day = "Mardi";
-        } else if (date("l", mktime(0,0,0, $dateparts[2], $dateparts[1], $dateparts[0])) == "Wednesday") {
+        } else if (date("l", mktime(0,0,0, $dateparts[1], $dateparts[2], $dateparts[0])) == "Wednesday") {
 
             $day = "Mercredi";
-        } else if (date("l", mktime(0,0,0, $dateparts[2], $dateparts[1], $dateparts[0])) == "Thursday") {
+        } else if (date("l", mktime(0,0,0, $dateparts[1], $dateparts[2], $dateparts[0])) == "Thursday") {
 
             $day = "Jeudi";
-        } else if (date("l", mktime(0,0,0, $dateparts[2], $dateparts[1], $dateparts[0])) == "Friday") {
+        } else if (date("l", mktime(0,0,0, $dateparts[1], $dateparts[2], $dateparts[0])) == "Friday") {
 
             $day = "Vendredi";
-        } else if (date("l", mktime(0,0,0, $dateparts[2], $dateparts[1], $dateparts[0])) == "Saturday") {
+        } else if (date("l", mktime(0,0,0, $dateparts[1], $dateparts[2], $dateparts[0])) == "Saturday") {
 
             $day = "Samedi";
-        } else if (date("l", mktime(0,0,0, $dateparts[2], $dateparts[1], $dateparts[0])) == "Sunday") {
+        } else if (date("l", mktime(0,0,0, $dateparts[1], $dateparts[2], $dateparts[0])) == "Sunday") {
 
             $day = "Dimanche";
         }
